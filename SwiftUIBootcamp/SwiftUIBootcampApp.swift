024fa4d041a54744441ed037b0f8f201f5113513
Arunrth <arunrthwm@gmail.com>
2024-10-23 22:45:09 +0530

    Initial Commit

---
+++ SwiftUIBootcamp/SwiftUIBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcampApp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Arun Rathore on 23/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIBootcampTests/SwiftUIBootcampTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcampTests.swift
+//  SwiftUIBootcampTests
+//
+//  Created by Arun Rathore on 23/10/24.
+//
+
+import Testing
+@testable import SwiftUIBootcamp
+
+struct SwiftUIBootcampTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
